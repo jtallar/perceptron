57243897d17761b8d56c43d9d6c3356347307393
Julian Vuoso <juvuoso@itba.edu.ar>
2021-04-16 12:54:41 -0300

    plot simple OK

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
 import sys
-import parser 
 import json
 import numpy as np
-import simple
-import linear
+import perceptron
 import parser
 
 # Read configurations from file
 with open("config.json") as file:
     config = json.load(file)
 
+# TODO: proteger de error / inexistente
 max_iter = config["max_iter"]
 eta = config["eta"]
 training_linear = config["training_file"]
 out_linear = config["expected_out"]
+is_linear = True if config["linear"]=="true" else False
+beta = config["beta"]
 
+# Testing Simple
 
-inputs = np.array([[1,1], [1,0], [0,1], [0,0]])
-outputs = np.array([1, 0, 0, 1])
-perceptron = simple.SimplePerceptron(max_iter, eta)
+inputs = np.array([[1,1], [1,-1], [-1,1], [-1,-1]])
+outputs = np.array([1, 1, 1, -1])
+perceptron = perceptron.SimplePerceptron(max_iter, eta)
 perceptron.train(inputs, outputs)
 print(perceptron.prediction(inputs)) 
 
 
-"""
+# Testing Linear or Non-Linear
+'''
+perceptron = perceptron.LinearOrNotPerceptron(max_iter, eta, is_linear, beta)
 t, o = parser.training(training_linear, out_linear)
-perceptron = linear.SimplePerceptron(max_iter, eta)
 perceptron.train(t, o)
 inputs = np.array([[4.4793, -4.0765, 4.4558], [-4.1793, -4.9218, 1.7664], [-3.9429, -0.7689, 4.8830], [-3.5796,1.5557,2.6683]])
 print(perceptron.prediction(inputs)) 
-"""
+'''
+# Testing Other
+'''
+inputs = [[1.0,1.0,0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0 , 1.0]]
+perceptron = linear.LinearOrNotPerceptron(max_iter, eta)
+perceptron.train(inputs, outputs)
 
-
-# inputs = [[1.0,1.0,0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0 , 1.0]]
-# perceptron = linear.SimplePerceptron(max_iter, eta)
-# perceptron.train(inputs, outputs)
-
-# print(perceptron.prediction(np.array(inputs))) 
+print(perceptron.prediction(np.array(inputs))) 
+'''
