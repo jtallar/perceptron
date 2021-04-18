d695b17028db38475b7c2b127f7fd346e3a27082
Matias Brula <mbrula@itba.edu.ar>
2021-04-18 00:12:42 -0300

    added 3D and 2D plotting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import math
+
 import numpy as np
 
 import parser
@@ -18,19 +20,32 @@ training_file_name: str = config["training_file"]
 expected_out_file_name: str = config["expected_out_file"]
 number_class = float if config["float_data"] else int
 threshold: int = config["threshold"]
-
-normalize_expected_out_file: bool = config["normalize_expected_out_file"]
+error_threshold: float = config["error_threshold"]
 
 randomize_w: bool = config["randomize_w"]
 randomize_w_refs: int = config["randomize_w_refs"]
 change_w_iterations: int = config["change_w_iterations"]
 
+steps_graph_3d: int = config["steps_graph_3d"]
+
 activation_functions_dict = {
-    "sign": lambda x: np.asarray(np.sign(x), number_class),
-    "linear": lambda x: x,
-    "nonlinear": lambda x: np.tanh(x * beta)
+    "sign": [lambda x: np.asarray(np.sign(x), number_class),
+             lambda x: 1],
+
+    "linear": [lambda x: x,
+               lambda x: 1],
+
+    "tanh": [lambda x: np.tanh(x * beta),
+             lambda x: beta * (1 - activation_functions_dict["tanh"][0](x) ** 2)],
+
+    "exp": [lambda x: 1 / (1 + math.e ** (-2 * beta * x)),
+            lambda x: 2 * beta * activation_functions_dict["exp"][0](x) * (1 - activation_functions_dict["exp"][0](x))]
+
 }
-activation_function = activation_functions_dict[config["system"]]
+
+# activation functions and boolean for normalize
+activation_functions = activation_functions_dict[config["system"]]
+normalize_expected_out_file: bool = (config["system"] == "tanh") | (config["system"] == "exp")
 
 
 # read the files
@@ -38,12 +53,15 @@ training, outputs = parser.read_files(training_file_name, expected_out_file_name
                                       number_class, normalize_expected_out_file, threshold)
 
 # initialize perceptron
-perceptron = perceptron.Perceptron(activation_function, len(training[0]))
+perceptron = perceptron.SimplePerceptron(activation_functions, len(training[0]))
 
 # train perceptron and print step count and w
-print(perceptron.train(training, outputs, max_iter, eta,
+print(perceptron.train(training, outputs, max_iter, eta, error_threshold,
                        randomize_w, randomize_w_refs, change_w_iterations))
 
 # print difference between the real output and the one from the perceptron
 print(perceptron.diff_predict_expected(outputs, training))
+
+# plot the graphic with the training set
+perceptron.plot(training, steps_graph_3d)
 
--- perceptron.py
+++ perceptron.py
@@ -1,17 +1,21 @@
+import math
 import random
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 # TODO: check for input and dimension consistency
-class Perceptron(object):
+class SimplePerceptron(object):
 
-    def __init__(self, activation_func, dimension: int):
-        self.act_func = activation_func
+    def __init__(self, activation_functions: [], dimension: int):
+        self.act_func = activation_functions[0]
+        self.act_func_der = activation_functions[1]
         self.dim: int = dimension
         self.w: np.ndarray = np.zeros(dimension)
 
     def train(self, training_data: np.ndarray, expected_out_data: np.ndarray,
-              max_iter: int, eta: float,
+              max_iter: int, eta: float, error_threshold: float = 0.0,
               random_w: bool = False, random_ref: int = 100, random_iter: int = 100):
 
         # rename and initialize some data
@@ -27,7 +31,7 @@ class Perceptron(object):
         w_min: np.ndarray = self.w
 
         # finish only when error is 0 or reached max iterations
-        while error > 0 and i < max_iter:
+        while error > error_threshold and i < max_iter:
 
             # in case there is random w, randomize it again
             if n > p * random_iter:
@@ -37,8 +41,8 @@ class Perceptron(object):
             # random index to analyze a training set
             x_i = random.randint(0, p - 1)
 
-            # recalculate w by adding the delta w
-            self.w += eta * self.diff_predict_expected(y[x_i], x[x_i]) * x[x_i]
+            # recalculate w by adding the delta w using activation derived and not
+            self.w += eta * (y[x_i] - self.predict(x[x_i])) * self.predict_der(x[x_i]) * x[x_i]
 
             # calculate error with the current w
             error = self.error(x, y)
@@ -53,25 +57,21 @@ class Perceptron(object):
             i += 1
             n += 1
 
-        if i >= max_iter:
-            print(f"Finished due to reaching {max_iter} iterations")
-
-        return i, self.w
-
-    # given the input, and the expected value, get the difference while applying the activation function
-    def diff_predict_expected(self, expected, input_data: np.ndarray) -> float:
-        return expected - self.predict(input_data)
+        return i, error, self.w
 
     # given an already trained (or not) perceptron and a input, get the prediction value/s made
     def predict(self, input_data: np.ndarray):
         return self.act_func(np.dot(input_data, self.w))
 
+    # predict derivative of the activation function
+    def predict_der(self, input_data: np.ndarray):
+        return self.act_func_der(np.dot(input_data, self.w))
+
     # calculate the error of the perceptron given a training and expected output set
     def error(self, x, y):
         aux = 0.0
         for xi, yi in zip(x, y):
-            # TODO: is abs always or when?
-            aux += abs(self.diff_predict_expected(yi, xi) ** 2)
+            aux += abs((yi - self.predict(xi)) ** 2)
         return aux / 2
 
     # initialize w array randomly or not
@@ -81,13 +81,63 @@ class Perceptron(object):
         else:
             return self.w
 
-# def plot(self, input):
-#     w_x = np.linspace(np.min(input)-1, np.max(input)+1, len(self.w))     # de donde a donde se dibuja la recta de W
-#     w_y = (-np.sign(self.w[0])*((self.w[2] + self.w[0] * w_x) / self.w[1]))
-#     plt.plot(w_x, w_y, color="green")
-#     for e in input:
-#         if e[1] > (-np.sign(self.w[0])*((self.w[2] + self.w[0] * e[0]) / self.w[1])):
-#             plt.scatter(e[0],e[1],color="red")
-#         else:
-#             plt.scatter(e[0],e[1],color="blue")
-#     plt.show()
+    # given the input, and the expected value, get the difference while applying the activation function
+    def diff_predict_expected(self, expected, input_data: np.ndarray) -> float:
+        return expected - self.predict(input_data)
+
+    # plot the solution
+    def plot(self, input_data: np.ndarray, steps: int = 20) -> ():
+
+        # check for valid plotting dimension
+        if 2 < self.dim - 1 > 3:
+            print(f"Cannot plot a {self.dim - 1} dimension perceptron")
+            exit(1)
+
+        def function(xi, yi, *argv):
+            return (self.w[0] + self.w[1] * xi + self.w[2] * yi) / (-self.w[-1])
+
+        # set the figure dimensions and axis labels
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d' if self.dim - 1 == 3 else None)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+
+        # get the top values for dim - 1 axis
+        max_array: np.ndarray = np.abs(input_data[0])
+        for point in input_data:
+            # do not take w0 and last one
+            for i in np.arange(1, len(point) - 1, 1):
+                if abs(point[i]) > max_array[i]:
+                    max_array[i] = abs(point[i])
+        # delete first (w0) and last
+        max_array = np.delete(max_array, [0, -1])
+
+        # generate range for dim - 1 axis
+        args_w_plot = []
+        for max_val in max_array:
+            # add a 10% of margin and grab ceiling
+            max_val: float = math.ceil(max_val * 1.15)
+            # divide by X the range
+            args_w_plot.append(np.linspace(-max_val, max_val, steps))
+
+        if self.dim - 1 == 3:
+            ax.set_zlabel('Z')
+            x, y = np.meshgrid(*tuple(args_w_plot))
+            z = function(x, y)
+            ax.plot_wireframe(x, y, z)
+        else:
+            x = args_w_plot[0]
+            y = function(x, 0)
+            ax.plot(x, y)
+
+        # print each point in the graphic
+        for point in input_data:
+            p_args: tuple = tuple(np.delete(point, 0))
+
+            if np.array(p_args)[-1] > function(*p_args, 0):
+                ax.scatter(*p_args, color="red")
+            else:
+                ax.scatter(*p_args, color="blue")
+
+        # plot
+        plt.show()
