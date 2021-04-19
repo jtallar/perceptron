some fixes on error calculation

main.py
@@ -19,7 +19,6 @@ beta: float = config["beta"]
 
 training_file_name: str = config["training_file"]
 expected_out_file_name: str = config["expected_out_file"]
-number_class = float if config["float_data"] else int
 threshold: int = config["threshold"]
 error_threshold: float = config["error_threshold"]
 
@@ -30,6 +29,18 @@ change_w_iterations: int = config["change_w_iterations"]
 complex_layout: [int] = config["complex_layout"]
 
 steps_graph_3d: int = config["steps_graph_3d"]
+
+
+# read the files
+number_class = float
+normalize_expected_out_file: bool = (config["system"] == "tanh") | (config["system"] == "exp")
+try:
+    training_data, expected_out_data = parser.read_files(training_file_name, expected_out_file_name,
+                                                         number_class, normalize_expected_out_file, threshold)
+except ValueError:
+    number_class = int
+    training_data, expected_out_data = parser.read_files(training_file_name, expected_out_file_name,
+                                                         number_class, normalize_expected_out_file, threshold)
 
 activation_functions_dict = {
     "sign": [lambda x: np.asarray(np.sign(x), number_class),
@@ -43,16 +54,10 @@ activation_functions_dict = {
 
     "exp": [lambda x: 1 / (1 + math.e ** (-2 * beta * x)),
             lambda x: 2 * beta * activation_functions_dict["exp"][0](x) * (1 - activation_functions_dict["exp"][0](x))]
-
 }
 
 # activation functions and boolean for normalize
 activation_functions = activation_functions_dict[config["system"]]
-normalize_expected_out_file: bool = (config["system"] == "tanh") | (config["system"] == "exp")
-
-# read the files
-training_data, expected_out_data = parser.read_files(training_file_name, expected_out_file_name,
-                                                     number_class, normalize_expected_out_file, threshold)
 
 # initialize the perceptron completely
 perceptron = perceptron.ComplexPerceptron(*tuple(activation_functions), complex_layout, len(training_data[0]))
@@ -82,8 +87,8 @@ while error > error_threshold and i < max_iter:
 
 print(perceptron)
 print(f"error is: {error_min}, iterations: {i}")
-for data in training_data:
-    print(perceptron.activation(np.array(data)))
+for data, out in zip(training_data, expected_out_data):
+    print(f"in: {data}, expected: {out}, out: {perceptron.activation(np.array(data))}")
 # finished
 
 

perceptron.py
@@ -35,10 +35,10 @@ class SimplePerceptron(object):
     # returns int or float depending on the input data and activation function
     def activation(self, input_arr: np.ndarray, training: bool = False):
         if training:
-            self.input = np.asarray(input_arr)
+            self.input = input_arr
 
         # activation for this neuron, could be int or float, or an array in case is the full dataset
-        return self.act_func(np.dot(self.input, self.w))
+        return self.act_func(np.dot(input_arr, self.w))
 
     # returns the derived activation value/s for the given input in this neuron
     # returns int or float depending on the input data and activation function
@@ -89,13 +89,14 @@ class ComplexPerceptron(object):
             sup_aux = np.asarray(sup_aux)
 
     # propagates input along the entire network
-    # in case of training, saves the input for later computation on retro propagation
+    # in case of training, saves  the input for later computation on retro propagation
     # returns the final activation value
     def activation(self, init_input: np.ndarray, training: bool = False):
         activation_values = init_input
         for layer in self.network:
             pool = multiprocessing.pool.ThreadPool(processes=len(layer))
             activation_values = pool.map(lambda s_p: s_p.activation(activation_values, training=training), layer)
+            activation_values = np.transpose(np.asarray(activation_values))
 
         return activation_values[0]
 
@@ -111,6 +112,8 @@ class ComplexPerceptron(object):
 
             # initialize (empty) level with its amount of perceptron
             self.network[level] = np.empty(shape=layout[level], dtype=SimplePerceptron)
+
+            # the dimension of the next level is set from the previous or the input data
             dim: int = layout[level - 1] if level != 0 else input_dim
 
             # create the corresponding amount of perceptron